d860c92af715c0d73a7370a4bc31d4929bdbd31f
eliuth <eliuth@gmaol.com>
2023-01-30 12:02:31 -0500

    se arreglaron algunos errores

--- index.js
+++
@@ -1,3 +0,0 @@
-fetch('http://localhost:5000/pantalones')
-    .then(data => data.json())
-        .then(data => console.log(data))
--- servidor.js
+++ servidor.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const path = require('path')
+app.use(express.static(__dirname + '/public'));
 const { productos } = require('./productos')
 app.use(cors())
-app.get('/', (req, res) => res.send(path(__dirname,'index.html')))
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname,'index.html')))
 app.get('/productos', (req, res) => res.send(JSON.stringify(productos)))
 app.get('/camisas', (req, res) => res.send(JSON.stringify(productos.Camisas)))
 app.get('/pantalones', (req, res) => res.send(JSON.stringify(productos.Pantalones)))
@@ -14,16 +16,3 @@ app.get('/celulares', (req, res) => res.send(JSON.stringify(productos.Celulares)
 
 app.listen(5000, () => console.log('El servidor se esta ejecutando👍'))
 
-/*    `<center><h1>Bienvenido a products api</h1></center>
-<hr>
-<h2>Productos:<a href="/productos">/Productos</a><h2/><br>
-<h2>Relojes:<a href="/reloj">/Reloj</a><h2/>
-<br>
-<h2>Celulares:<a href="/celulares">/Celulares</a><h2/>
-<br>
-<h2>Camisas:<a href="/camisas">/Camisas</a><h2/>
-<br>
-<h2>Pantalones:<a href="/pantalones">/Pantalones</a><h2/>
-<br>
-<h2>Electrodomesticos:<a href="/electrodomesticos">/Electrodomesticos</a><h2/>
-`*/
